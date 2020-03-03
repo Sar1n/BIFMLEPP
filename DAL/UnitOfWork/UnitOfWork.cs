a3d76c123d664d37fa6734b1c03de002ebd9c3dd
Sarin <ivan.maluk@gmail.com>
2020-03-03 19:38:08 +0200

    Added BLL project and UnitOfWork class

--- DAL/UnitOfWork/UnitOfWork.cs
+++ DAL/UnitOfWork/UnitOfWork.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace DAL.UnitOfWork
 {
-    public class Class1
-    {
-    }
+	class UnitOfWork
+	{
+	}
 }
